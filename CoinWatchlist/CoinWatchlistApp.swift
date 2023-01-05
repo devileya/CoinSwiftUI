df38fd8cd7bff2676889b419a1924f276c76e626
Arif Fadly Siregar <fadlyarif77@gmail.com>
2023-01-06 00:49:25 +0700

    Initial Commit

---
+++ CoinWatchlist/CoinWatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinWatchlistApp.swift
+//  CoinWatchlist
+//
+//  Created by Arif Siregar on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinWatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
